92116fae640a2288e8111706fa3fcc1c9083d50a
Guillaume Ramey <guillaume.ramey@gmail.com>
2018-09-24 11:24:35 +0200

    Initial Commit

---
+++ Bagarre/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bagarre
+//
+//  Created by Guillaume Ramey on 24/09/2018.
+//  Copyright © 2018 Guillaume Ramey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
